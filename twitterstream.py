62c2d15fb94c84a03fb2d383c4a33e743f7fa3f0
vincewide <vincentwiderberg@hotmail.com>
2018-03-08 22:18:20 +0100

    Add files via upload

---
+++ twitterstream.py
@@ -0,0 +1,62 @@
+import oauth2 as oauth
+import urllib2 as urllib
+
+# See assignment1.html instructions or README for how to get these credentials
+
+api_key = "	yiqjapKK3ykRhcdAFp0TeYY7H" 
+api_secret = "	rLQImRyvnv3LWEf2XT0zCYT8eAfq1KOoeGYA1JEtT4DKYLMxlp" 
+access_token_key = "	971120834931843072-IGLmfjlQUDNihZInPG2jQAt92GvXjDa" 
+access_token_secret = "	0VNrGTOjRwLSe0D8i5o1UVdVP02kyrNEVLICrVcmI7zIP"
+
+
+_debug = 0
+
+oauth_token    = oauth.Token(key=access_token_key, secret=access_token_secret)
+oauth_consumer = oauth.Consumer(key=api_key, secret=api_secret)
+
+signature_method_hmac_sha1 = oauth.SignatureMethod_HMAC_SHA1()
+
+http_method = "GET"
+
+
+http_handler  = urllib.HTTPHandler(debuglevel=_debug)
+https_handler = urllib.HTTPSHandler(debuglevel=_debug)
+
+'''
+Construct, sign, and open a twitter request
+using the hard-coded credentials above.
+'''
+def twitterreq(url, method, parameters):
+  req = oauth.Request.from_consumer_and_token(oauth_consumer,
+                                             token=oauth_token,
+                                             http_method=http_method,
+                                             http_url=url, 
+                                             parameters=parameters)
+
+  req.sign_request(signature_method_hmac_sha1, oauth_consumer, oauth_token)
+
+  headers = req.to_header()
+
+  if http_method == "POST":
+    encoded_post_data = req.to_postdata()
+  else:
+    encoded_post_data = None
+    url = req.to_url()
+
+  opener = urllib.OpenerDirector()
+  opener.add_handler(http_handler)
+  opener.add_handler(https_handler)
+
+  response = opener.open(url, encoded_post_data)
+
+  return response
+
+def fetchsamples():
+  url = "https://stream.twitter.com/1/statuses/sample.json"
+  parameters = []
+  response = twitterreq(url, "GET", parameters)
+  for line in response:
+    print(line)
+
+if __name__ == '__main__':
+  fetchsamples()
